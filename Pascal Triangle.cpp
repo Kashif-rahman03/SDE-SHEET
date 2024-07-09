213632f1f284f2c8f2fd5703880c3736f96d16fd
Kashif Rahman <95422841+Kashif-rahman03@users.noreply.github.com>
2024-07-09 23:42:41 +0530

    Update Pascal Triangle.cpp

--- Pascal Triangle.cpp
+++ Pascal Triangle.cpp
@@ -8,8 +8,9 @@ public:
             r[i].resize(i + 1);
             r[i][0] = r[i][i] = 1;
   
-            for (int j = 1; j < i; j++)
+            for (int j = 1; j < i; j++){
                 r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+            }
         }
         
         return r;
